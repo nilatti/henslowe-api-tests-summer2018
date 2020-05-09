d1ca9df865c9f67f0d973928268c461e9bdc8c6b
Aili Huber <alisha.huber@gmail.com>
2020-05-09 16:57:32 -0400

    removed unnecessary build files

--- build/test_project.sh
+++ build/test_project.sh
@@ -4,13 +4,6 @@ set -e -x
 #!/bin/bash
 set -e -x
 # rest of the script
-echo 'Building SIF Project'
-AGENT_INSTALL_DIR="/var/lib/go-agent/pipelines"
-WORKSPACE="$AGENT_INSTALL_DIR/$GO_PIPELINE_NAME"
-echo "workspace is $WORKSPACE"
-DOCKER_HOME="$WORKSPACE/docker"
-echo "Docker home is $DOCKER_HOME"
-
 
 echo 'running rspec'
-#rspec
+rspec
